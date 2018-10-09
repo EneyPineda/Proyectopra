75cf86ac0cb0c22ad9fcd02c6b12a26209aec469
PedroJPAdan <pedro.abmodel@gmail.com>
2018-10-09 16:44:19 -0500

    componente de estadisticas Jesus

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    HeaderComponent
+    HeaderComponent,
+    EstadisticasComponent
   ],
   imports: [
     BrowserModule
