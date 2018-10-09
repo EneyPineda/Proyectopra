f306c0476b133bae6df616d315e26dfde87a73eb
EneyPineda <mayra_eneydi.1@hotmail.com>
2018-10-09 00:05:10 -0500

    Componentes

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
