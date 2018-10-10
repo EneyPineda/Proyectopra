8b8c999d678920dbaa7b595284b6455d022f1dbb
ErickZCortes <Erickccortes@hotmail.com>
2018-10-09 21:56:09 -0500

    menu

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
 import { EstadisticasComponent } from './estadisticas/estadisticas.component';
 import { AgregarComponent } from './agregar/agregar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     HeaderComponent,
-    AgregarComponent
+    AgregarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
