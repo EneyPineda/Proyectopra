0679ac50b06161f19c531a4d2d98ed465626f054
ErickZCortes <Erickccortes@gmail.com>
2018-11-04 15:00:04 -0600

    Mod-app

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {Routes, RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { PrincipalComponent } from './principal/principal.component';
 import { AgregarComponent } from './agregar/agregar.component';
+import { LoginComponent } from './login/login.component';
 const routes: Routes = [
     {
         path: '', redirectTo: '/principal', pathMatch: 'full'
@@ -11,12 +12,15 @@ const routes: Routes = [
     },
     {
         path: 'agregar', component: AgregarComponent
-    }    
+    },
+    {
+        path: 'login', component: LoginComponent
+    }
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule{
-    
+export class AppRoutingModule {
+
 }
--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
 import { AgregarComponent } from './agregar/agregar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     HeaderComponent,
-    AgregarComponent
+    AgregarComponent,
+    LoginComponent
     ],
   imports: [
     BrowserModule,
