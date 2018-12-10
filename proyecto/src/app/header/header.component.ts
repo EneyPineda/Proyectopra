29934b6ea9443bf61dd6a07e4e791df12a72ae35
PedroJPAdan <pedro.abmodel@gmail.com>
2018-12-10 04:52:09 -0600

    header html y ts.

--- proyecto/src/app/header/header.component.ts
+++ proyecto/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +12,38 @@ export class HeaderComponent implements OnInit {
 
 //  @Output() featureSelected = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(private authService: AuthService, private afsAuth: AngularFireAuth, private router: Router) { }
 
   /*onSelect(feature: string){
     this.featureSelected.emit(feature);
   }
 */
+
+  // tslint:disable-next-line:no-inferrable-types
+  public app_name: string = '';
+  // tslint:disable-next-line:no-inferrable-types
+  public isLogged: boolean = false;
+
   ngOnInit() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    this.authService.isAuth().subscribe(auth => {
+      if (auth) {
+        this.isLogged = true;
+        //console.log(this.isLogged);
+        //console.log(this.authService.getIdUSer());
+      } else if(!auth){
+        //console.log(this.isLogged);
+        this.isLogged = false;
+      }
+    });
+  }
+
+  onLogoutUser() {
+    this.authService.logoutUser();
+    this.router.navigate(['/principal']);
   }
 
 }
